01b54b33703e757300c4cb000b2acdb0aee9b01f
BykovIA <igor.bykov.96@bk.ru>
2020-06-30 02:12:26 +0500

    Update testScript.groovy

--- scripts/testScript.groovy
+++ scripts/testScript.groovy
@@ -11,8 +11,8 @@ pipeline {
                         passwordVariable: 'password')
                     ]) {
                         try {
-                            sh "echo '${password}' | sudo -S docker stop dcImgBIA"
-                            sh "echo '${password}' | sudo -S docker container rm dcImgBIA"
+                            sh "echo '${password}' | sudo -S docker stop dc_img_bia"
+                            sh "echo '${password}' | sudo -S docker container rm dc_img_bia"
                         } catch (Exception e) {
                             print '1st time? lets scip'
                         }
@@ -38,8 +38,8 @@ pipeline {
                         usernameVariable: 'username',
                         passwordVariable: 'password')
                     ]) {
-                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t bykovImgNginx"
-                        sh "echo '${password}' | sudo -S docker run -d -p 5757:80 --name dcImgBIA -v /home/adminci/is_mount_dir:/stat bykovImgNginx"
+                        sh "echo '${password}' | sudo -S docker build ${WORKSPACE}/auto -t bykov_img_nginx"
+                        sh "echo '${password}' | sudo -S docker run -d -p 5757:80 --name dc_img_bia -v /home/adminci/is_mount_dir:/stat bykov_img_nginx"
                     }
                 }
             }
@@ -53,8 +53,8 @@ pipeline {
                         passwordVariable: 'password')
                     ]) {
                         
-                        sh "echo '${password}' | sudo -S docker exec -t dcImgBIA bash -c 'df -h > /stat/stats.txt'"
-                        sh "echo '${password}' | sudo -S docker exec -t dcImgBIA bash -c 'top -n 1 -b >> /stat/stats.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t dc_img_bia bash -c 'df -h > /stat/stats.txt'"
+                        sh "echo '${password}' | sudo -S docker exec -t dc_img_bia bash -c 'top -n 1 -b >> /stat/stats.txt'"
                     }
                 }
             }
